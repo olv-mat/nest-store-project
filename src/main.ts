ab4ad6929ca7a3057c1b1812ecfcd108108abec5
Matheus Oliveira <contamatheusprofissional@gmail.com>
2025-07-01 16:05:55 -0300

    feat: swagger initial configs

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,26 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { useContainer } from 'class-validator';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { SwaggerTheme, SwaggerThemeNameEnum } from 'swagger-themes';
 
 async function bootstrap() {
+
     const app = await NestFactory.create(AppModule);
+    const config = new DocumentBuilder()
+        .setTitle("Nest Generic Store Backend")
+        .setDescription("This Is a Test Application For My NestJS Studies")
+        .setVersion("1.0")
+        .addBearerAuth()
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    const theme = new SwaggerTheme();
+    const darkTheme = theme.getBuffer(SwaggerThemeNameEnum.DARK);
+
+    SwaggerModule.setup("api", app, document, {
+        customCss: darkTheme,
+    });
+
     app.useGlobalPipes(
         new ValidationPipe({
             transform: true,
--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -2,7 +2,9 @@ import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common"
 import { CreateUserDTO } from "./dtos/CreateUser.dto";
 import { UpdateUserDTO } from "./dtos/UpdateUser.dto";
 import { UserService } from "./user.service";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 
+@ApiTags("users")
 @Controller("users")
 export class UserController {
 
@@ -11,21 +13,29 @@ export class UserController {
     ) { }
 
     @Get()
+    @ApiOperation({ summary: "List registered users" })
+    @ApiResponse({ status: 200, description: "Users returned successfully" })
     public async listUsers() {
         return await this.userService.listUsers();
     }
 
     @Post()
+    @ApiOperation({ summary: "Register a user" })
+    @ApiResponse({ status: 200, description: "User registered successfully" })
     public async createUser(@Body() request: CreateUserDTO) {
         return await this.userService.createUser(request);
     }
 
     @Put("/:id")
+    @ApiOperation({ summary: "Update a user" })
+    @ApiResponse({ status: 200, description: "Users updated successfully" })
     public async updateUser(@Param("id") id: string, @Body() request: UpdateUserDTO) {
         return await this.userService.updateUser(id, request);
     }
 
     @Delete("/:id")
+    @ApiOperation({ summary: "Delete a user" })
+    @ApiResponse({ status: 200, description: "Users deleted successfully" })
     public async deleteUser(@Param("id") id: string) {
         return await this.userService.deleteUser(id);
     }
